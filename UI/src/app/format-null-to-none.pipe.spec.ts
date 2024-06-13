673b43a67210ccc988a2183d9ed057fffe09d0c7
Anand Inedi <anand.i@Technovert.com>
2024-06-13 10:46:39 +0530

    Add File Structure

--- UI/src/app/format-null-to-none.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatNullToNonePipe } from './format-null-to-none.pipe';
-
-describe('FormatNullToNonePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatNullToNonePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
